5709de4b626a581dc38c0a0a40a7b3fa75476362
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-02-07 01:22:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,22 +11,35 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+<<<<<<< HEAD
     @user = User.find_by(username: params[:username])
     if @user 
       session[:user_id] = @user.id 
       redirect "/account"
     else 
       erb :error
+=======
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user.class == User
+      session[:user_id] = @user.id
+    redirect "/account"
+    else
+      redirect "/error"
+>>>>>>> d9b05580f85c86a8a4bdca5c4a99d1ee10d77c76
     end
   end
   
   get '/account' do
+<<<<<<< HEAD
     @logged_user = User.find_by_id(session[:user_id])
     if @logged_user
       erb :account
     else
        erb :error
     end
+=======
+    
+>>>>>>> d9b05580f85c86a8a4bdca5c4a99d1ee10d77c76
   end
 
   get '/logout' do
